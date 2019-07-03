c8c3c4a423441244993a94db19d95108cbd3490c
Katie Cawthorne <katie.cawthorne@digital.justice.gov.uk>
2019-07-03 10:11:20 +0100

    #562 removed hard coded port number

--- src/server.mjs
+++ src/server.mjs
@@ -1,15 +1,16 @@
 import app from './index.mjs';
 import * as routes from './routes/index.mjs';
+import { config } from './config.mjs';
 
 // Add top-level routes
-app.use(routes.home);
+app.use(routes.list);
 
 
 // Add nested routes
 app.use('/offender', routes.offender);
 
 // Start on port
-app.listen(3000);
+app.listen(config.port);
 
 // Export server (with routes)
 export default app;
