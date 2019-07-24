d0da3c057d4d0f36fbec2377ccab98dc70eb5ada
Andy Marke <andy.marke@gmail.com>
2019-07-24 17:00:13 +0100

    665 Use dynamic data

--- src/routes/list/index.mjs
+++ src/routes/list/index.mjs
@@ -5,7 +5,53 @@ const router = new express.Router();
 
 // Home page
 router.get('/', async (req, res) => {
-  res.render('list/views/list');
+  const data = {
+    courtHouse: 'Sheffield Magistrates Court',
+    dateOfAppearance: 'Wednesday, 24th July 2019',
+    cases: [
+      {
+        listingNumber: 3,
+        blockDescription: '10:00-10:30',
+        defendant: {
+          name: 'John Smith',
+        },
+        offences: [
+          {
+            title: 'Assault by beating',
+          },
+        ],
+        delius: {
+          status: 'Current (nps)',
+        },
+        session: {
+          courtRoom: '2',
+          startTime: '10:00',
+          endTime: '10:30',
+        },
+      },
+      {
+        listingNumber: 2,
+        blockDescription: '10:00-10:30',
+        defendant: {
+          name: 'Micky Smith',
+        },
+        offences: [
+          {
+            title: 'Assault by beating',
+          },
+        ],
+        delius: {
+          status: 'Known',
+        },
+        session: {
+          courtRoom: '1',
+          startTime: '10:20',
+          endTime: '10:40',
+        },
+      },
+    ],
+  };
+  res.render('list/views/list', data);
 });
 
 // Export as router
